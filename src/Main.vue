86f58002a43fdb02fdd774f583efb67e5f5fd1d4
chunill <yces980096@gmail.com>
2022-10-05 00:48:28 +0800

    feat: add FrontPage.vue and Main.vue

---
+++ src/Main.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import FrontPage from './components/FrontPage.vue'
+</script>
+
+<template>
+    <FrontPage msg="test" />
+</template>
+
+<style scoped></style>
---
+++ src/components/FrontPage.vue
@@ -0,0 +1,6 @@
+<script setup>
+    const title = "阿千的雜物堆"
+</script>
+<template>
+    {{ title }}
+</template>
