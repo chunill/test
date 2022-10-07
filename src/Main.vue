2783c65f7bc20e47f607771b25c874eb8268fbb6
chunill <yces980096@gmail.com>
2022-10-08 02:13:46 +0800

    feat: create PageHand.vue components

--- src/Main.vue
+++ src/Main.vue
@@ -1,9 +1,7 @@
 <script setup>
-    import FrontPage from './components/FrontPage.vue'
 </script>
 
 <template>
-    <FrontPage />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import Main from "./Main.vue"
+import PageHand from "./PageHand.vue"
 
 createApp(Main).mount('#main')
+createApp(PageHand).mount('#pageHand')
