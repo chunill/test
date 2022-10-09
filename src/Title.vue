34121f18b4625b3eeeafc81f9413433ae1cb9d82
chunill <yces980096@gmail.com>
2022-10-09 23:14:29 +0800

    refactor: fix title.vue css

--- src/Title.vue
+++ src/Title.vue
@@ -3,15 +3,22 @@
 
 <template>
     <div class="titleBlock">
-        <h1 id="pageTitle"> 阿千的雜物堆 </h1>
+        <h1 class="pageText"> 阿千的雜物堆 </h1>
     </div>
     <br>
     <div class="titleBlock">
-        <p id="pageTitle"> test </p>
+        <p class="pageText">  </p>
     </div>
 </template>
 
 <style scoped>
+    p, h1 {
+        position: relative;
+        top: 30%;
+        color: black;
+        animation: title 2s ease-in-out;
+    }
+
     .titleBlock {
         width: 700px;
         height: 150px;
@@ -20,13 +27,6 @@
         border-radius: 4px;
     }
 
-    #pageTitle {
-        position: relative;
-        top: 30%;
-        padding: 0;
-        color: black;
-        animation: title 2s ease-in-out;
-    }
     @keyframes title{
         0% {opacity: 0;}
         100%{opacity: 1;}
