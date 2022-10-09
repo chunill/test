f91b2890b174a90e87457439844181422558528f
chunill <yces980096@gmail.com>
2022-10-10 01:12:21 +0800

    feat: add about me img

---
+++ src/AboutMe.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <input type="image" src="/log4-N273/aboutMe.jpg" id="aboutMeButton" />
+</template>
+
+<style scoped>
+    #aboutMeButton {
+        width: 100%;
+        height: 100%;
+        border-radius: 800em;
+    }
+
+    @keyframes title{
+        0% {opacity: 0;}
+        100%{opacity: 1;}
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import Title from "./Title.vue"
 import Menu from "./Menu.vue"
+import AboutMe from "./AboutMe.vue"
 
 createApp(Title).mount('#title')
 createApp(Menu).mount('#menu')
+createApp(AboutMe).mount('#aboutMe')
