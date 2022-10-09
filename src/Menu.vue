8bd4aa32431790ff0c5b3168e1be44ef204a4a56
chunill <yces980096@gmail.com>
2022-10-09 21:37:09 +0800

    feat: add menu button img

--- src/Menu.vue
+++ src/Menu.vue
@@ -1,3 +1,10 @@
 <template>
-    
+    <img src='./assets/menuButton.png' id="menuButton"/>
 </template>
+
+<style scoped>
+#menuButton {
+    width: 50px;
+    height: 50px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import Title from "./Title.vue"
+import Menu from "./Menu.vue"
 
 createApp(Title).mount('#title')
+createApp(Menu).mount('#menu')
